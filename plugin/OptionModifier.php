76053596ec47267f11175dc17072a9c58a65a7dd
wj008 <26029682@qq.com>
2018-07-26 22:40:09 +0800

    添加 number_format

--- plugin/OptionModifier.php
+++ plugin/OptionModifier.php
@@ -5,18 +5,18 @@ namespace sdopx\plugin;
 
 class OptionModifier
 {
-    public  function render($string, $arg1, $arg2, $def = '')
+    public function render($string, $keys, $values, $def = '')
     {
         if (empty($string)) {
             return $def;
         }
-        if (is_array($arg1) && is_array($arg2)) {
-            $key = array_search($string, $arg1);
+        if (is_array($keys) && is_array($values)) {
+            $key = array_search($string, $keys);
             if ($key === false) {
                 return $def;
             }
-            return array_key_exists($key, $arg2) ? $arg2[$key] : $def;
+            return array_key_exists($key, $values) ? $values[$key] : $def;
         }
-        return $string == $arg1 ? $arg2 : $def;
+        return $string == $keys ? $values : $def;
     }
 }
