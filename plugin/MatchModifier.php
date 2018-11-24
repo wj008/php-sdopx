294311c5516d581f83ea4230288153f27373c5ee
wj008 <26029682@qq.com>
2018-11-24 17:54:54 +0800

    修改命名，Option 改成 Match

--- plugin/MatchModifier.php
+++ plugin/MatchModifier.php
@@ -1,27 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: wj008
- * Date: 18-7-26
- * Time: 下午10:32
- */
 
 namespace sdopx\plugin;
 
 
 class MatchModifier
 {
-    public function render($item, $option, $def = '')
+    public function render($string, $keys, $values, $def = '')
     {
         if (empty($string)) {
             return $def;
         }
-        if (is_array($option) && (is_string($item) || is_numeric($item))) {
-            if (isset($option[$item])) {
-                return $option[$item];
+        if (is_array($keys) && is_array($values)) {
+            $key = array_search($string, $keys);
+            if ($key === false) {
+                return $def;
             }
-            return $def;
+            return array_key_exists($key, $values) ? $values[$key] : $def;
         }
-        return $item == $option ? $option : $def;
+        return $string == $keys ? $values : $def;
     }
 }
