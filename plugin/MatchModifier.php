d2b9217ab1152c3f57776b46ae506c3b24039ff6
wj008 <26029682@qq.com>
2018-11-25 21:04:50 +0800

    调整 match 修饰器支持 match:[1,2]:['a','b']:'def' match:[1=>'a',2=>'b']:'def' 两种写法

--- plugin/MatchModifier.php
+++ plugin/MatchModifier.php
@@ -5,17 +5,29 @@ namespace sdopx\plugin;
 
 class MatchModifier
 {
-    public function render($string, $keys, $values, $def = '')
+    public function render($string, $keys, $values = null, $def = '')
     {
-        if (empty($string)) {
-            return $def;
-        }
+        //如果两个都是数组
         if (is_array($keys) && is_array($values)) {
+            if (empty($string)) {
+                return $def;
+            }
             $key = array_search($string, $keys);
             if ($key === false) {
                 return $def;
             }
             return array_key_exists($key, $values) ? $values[$key] : $def;
+        } else if (is_array($keys)) {
+            $def = $values;
+            if (empty($string)) {
+                return $def;
+            }
+            foreach ($keys as $k => $v) {
+                if ($k == $string) {
+                    return $v;
+                }
+            }
+            return $def;
         }
         return $string == $keys ? $values : $def;
     }
